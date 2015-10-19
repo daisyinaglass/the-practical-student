41de7a9cb594bf8e36902c4380af476409e4a79f
Sarah Hendricks <slynnhendricks@gmail.com>
2015-10-19 23:45:07 +0800

    Move adding rows to own document
    Unsure how to have data submit through and then return to a page:
    -curent page: don’t know how to do
    -user profile: what if there is an error in creating the profile?

--- Assignment 2/profile.php
+++ Assignment 2/profile.php
@@ -7,43 +7,7 @@
 	<meta>
 </head>
 <body>
-	<?php
-		function cleanData($text) {
-			$text = trim($text);
-			$text = stripcslashes($text);
-			$text = htmlspecialchars($text);
-		}
-		//get the data sent over
-		$firstname = $_POST["firstname"];
-		$lastname = $_POST["lastname"];
-		$email = $_POST["email"];
-		$username = $_POST["username"];
-		$password = $_POST["password"];
-		//clean it up
-		cleanData($firstname);
-		cleanData($lastname);
-		cleanData($email);
-		cleanData($username);
-		cleanData($password);
-		
-		$host = "localhost";
-	    $user = "X32720502";
-	    $password = "X32720502";
-	    $dbc = mysql_pconnect($host, $user, $password);
-	    $dbname = "X32720502";
-	    mysql_select_db($dbname) or die("Cannot connect to database ".mysql_error());
-
-	    //construct the query string
-	    $query = "INSERT INTO USERS VALUES ('".$username."', '".$password."', '".$firstname."', '".$lastname."', '".$email."');";
-	    
-	    if(!mysql_query($query, $dbc)) {
-	    	die('Error: '.mysql_error());
-	    }
-	    echo "<script>alert(\"Your info was added to the database\")</script>";
-
-	    mysql_free_result($result);
-	    mysql_close();	
-	?>
+	
 	<header>
 		<div id="Logo">
 		</div>
