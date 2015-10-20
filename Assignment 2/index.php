b48e9488b7b6c9946526398ef46abd87fbf5181d
Sarah Hendricks <slynnhendricks@gmail.com>
2015-10-20 16:31:35 +0800

    Add product links to items on index page

--- Assignment 2/index.php
+++ Assignment 2/index.php
@@ -49,7 +49,7 @@
 	</header>
 
 	<div id="content">
-		<a href="style.css" id="jumbo-link">
+		<a href="search.php?searchtext=Notelook&searchbutton=Search" id="jumbo-link">
 			<div id="jumbo">
 				<div class="container">
 					<h2>Introducing</h2>
@@ -68,17 +68,17 @@
 				<h1>Paper Products</h1>
 				<div class="col span-1-3">
 					<img src="images/planner.jpg">
-					<h2>Large Weekly Planner</h2>
+					<h2><a href="display.php?Name=Large%20Weekly%20Planner">Large Weekly Planner</a></h2>
 					<p>Perfect for organizing all of your classes, meetings, and fun!</p>
 				</div>
 				<div class="col span-1-3">
 					<img src="images/college-app.jpg">
-					<h2>College Application Worksheet</h2>
+					<h2><a href="display.php?Name=College%20Application%20Worksheets">College Application Worksheet</a></h2>
 					<p>Still not sure where you want to go? We'll help you find the school that's right for <i>you</i>!</p>
 				</div>
 				<div class="col span-1-3">
 					<img src="images/budget.jpg">
-					<h2>Budget Sheet</h2>
+					<h2><a href="display.php?Name=Budget%20Sheets">Budget Sheet</a></h2>
 					<p>Being a college student is synonymous with living on Ramen and peanut butter. Make it easier on yourself by tracking your finances with our handy sheets.</p>
 				</div>
 			</div>
@@ -86,12 +86,12 @@
 				<h1>Writing Utensils</h1>
 				<div class="col span-1-2">
 					<img src="images/highlighter.jpg">
-					<h2>Pastel Highlighter Set</h2>
+					<h2><a href="display.php?Name=Highlighter%20Set">Pastel Highlighter Set</a></h2>
 					<p>Spring has sprung! Give your notes some life with our fantastic highlighter pens.</p>
 				</div>
 				<div class="col span-1-2">
 					<img src="images/pens.jpg">
-					<h2>Gel Pen Set</h2>
+					<h2><a href="display.php?Name=Pen%20Set">Pen Set</a></h2>
 					<p>Perfect for class, and in enough colors to make taking notes a pleasure rather than a chore.</p>
 					<!-- <p>You're looking in your bag for your favorite pen, but all you find is... ugh, one of those cheap ones from orientation. Grab our set of easy-flowing, fast-drying pens and never be without your lucky pen again!</p> -->
 				</div>
@@ -100,29 +100,29 @@
 				<h1>Keep Organized</h1>
 				<div class="col span-1-4">
 					<img src="images/desk-organizer.jpg">
-					<h2>Desktop Organizer</h2>
+					<h2><a href="display.php?Name=Desktop%20Organizer">Desktop Organizer</a></h2>
 					<p>We all need a study space. Keep yours neat and clean with this organizer.</p>
 				</div>
 				<div class="col span-1-4">
 					<img src="images/college-degree-worksheet.jpg">
-					<h2>College Degree Worksheets</h2>
+					<h2><a href="display.php?Name=College%20Degree%20Worksheets">College Degree Worksheets</a></h2>
 					<p>Make sure you graduate on time! Plan your college years here so that you don't miss a thing.</p>
 				</div>
 				<div class="col span-1-4">
 					<img src="images/whiteboard.jpg">
-					<h2>Whiteboard Set</h2>
+					<h2><a href="display.php?Name=White%20Board%20Set">Whiteboard Set</a></h2>
 					<p>Drilling vocabulary, planning meals, keeping motivated... whatever you need to change regularly, our whiteboard set can handle it!</p>
 				</div>
 				<div class="col span-1-4">
 					<img src="images/binder.jpg">
-					<h2>Binders</h2>
+					<h2><a href="display.php?Name=3-Ring%20Binder">Binders</a></h2>
 					<p>There's only so much you can keep in a trapper before it starts falling apart, so use one of our high-quality binders instead.</p>
 				</div>
 			</div>
 		</div>
 		<div id="story">
 			<div class="container">
-				<p>What started in a college grad's garage has turned into something beyond our wildest dreams. Find out how we got started and how we plan to move forward <a href="">here!</a></p>
+				<p>What started in a college grad's garage has turned into something beyond our wildest dreams. Find out how we got started and how we plan to move forward <a href="about.html">here!</a></p>
 			</div>
 		</div>
 		<div id="testimonials">
