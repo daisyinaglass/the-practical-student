4e894a5dd7f459b41645d602f4f7b8e4b87d4a43
Sarah Hendricks <slynnhendricks@gmail.com>
2015-10-22 12:54:38 +0800

    Add updateProduct, deleteProduct, & viewUser pages

---
+++ Assignment 2/updateProduct.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" />
+<head>
+	<title>The Practical Student</title>
+	<meta charset="utf-8" />
+	<link type="text/css" rel="stylesheet" href="style.css" />
+	<link rel="stylesheet" type="text/css" href="form-style.css">
+	<script type="text/javascript" src="checkSearch.js"></script>
+	<script type="text/javascript" src="checkAddProduct.js"></script>
+</head>
+<body>
+	<header>
+		<div id="Logo">
+		</div>
+		<nav>
+			<div id="general-nav">
+				<ul>
+					<div id="float-left">
+						<form action="search.php" method="get" onsubmit="return checkSearch(this)" name="search-box">
+							<li><a href="index.php">Home</a></li>
+							<div class="search-box">
+								<input type="text" id="searchtext" name="searchtext" value="<?php echo $_SESSION['prevsearch'];?>" />
+								<input type="submit" id="submit" value="Search" name="searchbutton" />
+							</div>
+						</form>
+					</div>
+					<div id="float-right">
+						<li><a href="register.php">Register</a></li>
+						<li id="sign-in"><a href="login.php">Sign In</a></li>
+						<li><a href="cart.php">Cart</a></li>
+						<li><a href="help.php">Help</a></li>
+					</div>
+				</ul>
+			</div>
+			<div id="product-nav">
+				<ul>
+					<li><a href="search.php?searchtext=&searchbutton=Search">All Products</a></li>
+					<li><a href="search.php?searchtext=notelook">Notelooks</a></li>
+					<li><a href="search.php?searchtext=writing">Writing</a></li>
+					<li><a href="search.php?searchtext=bag">Backpacks</a></li>
+					<li><a href="search.php?searchtext=organization">Organization</a></li>
+					<li><a href="search.php?searchtext=planner">Planners</a></li>
+				</ul>
+			</div>
+		</nav>
+	</header>
+
+	<div id="content">
+		<?php 
+			$host = "localhost";
+		    $user = "X32720502";
+		    $password = "X32720502";
+		    $dbc = mysql_pconnect($host, $user, $password);
+		    $dbname = "X32720502";
+		    mysql_select_db($dbname) or die("Cannot connect to database ".mysql_error());
+
+		    //construct the query string
+		    $query = "SELECT * FROM USERS WHERE ProductID='".$productid."';";
+		    $result = mysql_query($query);
+			
+			print "<div class=\"form-box\">";
+			while ($row = mysql_fetch_array($result)) {
+				print "<h1>Update Product</h1>
+				<form id=\"updateproduct\" action=\"changeProduct.php\" method=\"post\" onsubmit=\"return checkAddProduct(this)\">
+					<input type=\"text\" value=\"".$row['Name']."\" name=\"name\" id=\"name\" /><br />
+					<input type=\"text\" value=\"".$row['Price']."\" name=\"price\" id=\"price\" /><br />
+					<input type=\"text\" value=\"".$row['PhotoReference']."\" name=\"photoref\" id=\"photoref\" /><br />
+					<input type=\"text\" value=\"".$row['Category']."\" name=\"category\" id=\"category\" /><br />
+					<textarea value=\"".$row['Description']."\" name=\"description\" id=\"description\"></textarea><br />
+					<input type=\"submit\" value=\"Update\" />
+				</form>";
+			}
+			print "</form>";
+			mysql_close();
+		?>
+
+			
+	</div>
+
+	<footer>
+		<ul>
+			<li><a href="about.php">About</a></li>
+			<li><a href="contact.php">Contact</a></li>
+			<li><a href="http://practicalstudent.tumblr.com/">Follow</a></li>
+		</ul>
+	</footer>
+</body>
+</html>
---
+++ Assignment 2/viewUser.php
@@ -0,0 +1,94 @@
+<?php
+session_start();
+include('session.php');
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" />
+<head>
+	<title>The Practical Student</title>
+	<meta charset="utf-8" />
+	<link type="text/css" rel="stylesheet" href="style.css" />
+	<script type="text/javascript" src="checkSearch.js"></script>
+	<script type="text/javascript" src="validateUpdate.js"></script>
+	<link rel="stylesheet" type="text/css" href="form-style.css">
+</head>
+<body>
+	<header>
+		<div id="Logo">
+		</div>
+		<nav>
+			<div id="general-nav">
+				<ul>
+					<div id="float-left">
+						<form action="search.php" method="get" onsubmit="return checkSearch(this)" name="search-box">
+							<li><a href="index.php">Home</a></li>
+							<div class="search-box">
+								<input type="text" id="searchtext" name="searchtext" value="<?php echo $_SESSION['prevsearch'];?>" />
+								<input type="submit" id="submit" value="Search" name="searchbutton" />
+							</div>
+						</form>
+					</div>
+					<div id="float-right">
+						<li><a href="register.php">Register</a></li>
+						<li id="sign-in"><a href="login.php">Sign In</a></li>
+						<li><a href="cart.php">Cart</a></li>
+						<li><a href="help.php">Help</a></li>
+					</div>
+				</ul>
+			</div>
+			<div id="product-nav">
+				<ul>
+					<li><a href="search.php?searchtext=&searchbutton=Search">All Products</a></li>
+					<li><a href="search.php?searchtext=notelook">Notelooks</a></li>
+					<li><a href="search.php?searchtext=writing">Writing</a></li>
+					<li><a href="search.php?searchtext=bag">Backpacks</a></li>
+					<li><a href="search.php?searchtext=organization">Organization</a></li>
+					<li><a href="search.php?searchtext=planner">Planners</a></li>
+				</ul>
+			</div>
+		</nav>
+	</header>
+
+	<div id="content">
+		<p>User Profile</p>
+		<?php 
+			$host = "localhost";
+		    $user = "X32720502";
+		    $password = "X32720502";
+		    $dbc = mysql_pconnect($host, $user, $password);
+		    $dbname = "X32720502";
+		    mysql_select_db($dbname) or die("Cannot connect to database ".mysql_error());
+
+		    //construct the query string
+		    $query = "SELECT * FROM USERS WHERE UserID='".$userid."';";
+		    $result = mysql_query($query);
+			
+			print "<div class=\"form-box\">";
+			while ($row = mysql_fetch_array($result)) {
+				print "<form>
+				First Name<input type=\"text\" value=\"".$row['FirstName']."\" name=\"firstname\" id=\"firstname\" readonly /><br />
+				Last Name<input type=\"text\" value=\"".$row['LastName']."\" name=\"lastname\" id=\"lastname\" readonly /><br />
+				Email <input type=\"email\" value=\"".$row['Email']."\" name=\"email\" id=\"email\" readonly /><br />
+				Address Line 1 <input type=\"text\" value=\"".$row['AddressLine1']."\" name=\"addressline1\" id=\"addressline1\" readonly /><br />
+				Address Line 2 <input type=\"text\" value=\"".$row['AddressLine2']."\" name=\"addressline2\" id=\"addressline2\" readonly /><br />
+				City <input type=\"text\" value=\"".$row['City']."\" name=\"city\" id=\"city\" readonly /><br />
+				State <input type=\"text\" value=\"".$row['State']."\" name=\"state\" id=\"state\" readonly /><br />
+				Zipcode <input type=\"text\" value=\"".$row['Zip']."\" name=\"zip\" id=\"zip\" readonly /><br />
+				Username <input type=\"text\" value=\"".$row['Username']."\" name=\"username\" id=\"username\" readonly/><br />
+				Password <input type=\"password\" value=\"".$row['Password']."\" name=\"password\" id=\"password\" readonly /><br />
+				</form>";
+			}
+			print "</form>";
+			mysql_close();
+		?>
+	</div>
+
+	<footer>
+		<ul>
+			<li><a href="about.php">About</a></li>
+			<li><a href="contact.php">Contact</a></li>
+			<li><a href="http://practicalstudent.tumblr.com/">Follow</a></li>
+		</ul>
+	</footer>
+</body>
+</html>
