6c8956f3c2bff2680ca4dc08c14ba2bed882a950
Sarah Hendricks <slynnhendricks@gmail.com>
2015-10-23 05:32:36 +0800

    Allow sendOrder.php to update database & send alerts

--- Assignment 2/sendOrder.php
+++ Assignment 2/sendOrder.php
@@ -15,14 +15,29 @@ foreach($_SESSION['cart'] as $row => $innerArray){
 	$qty = $innerArray[1];
 
 	//construct the query string
-	$query = "SELECT Stock FROM PRODUCTS WHERE ProductID='".$productid."';";
+	$query = "SELECT * FROM PRODUCTS WHERE ProductID='".$productid."';";
 	$result = mysql_query($query);
+	//print mysql_num_rows($result);
 
+	//if user exceeds amount, write alert
 	while ($row = mysql_fetch_array($result)) {
+		echo $qty;
 		echo $row['Stock'];
+		if ($row['Stock'] - $qty < 0) {
+			echo "<script>alert(\"We\'ve run out of ".$row['Name']."!<br>Please allow two weeks for restocking and the shipment of your other ".$qty-$row['Stock']." ".$row['Name'].".\");</script>";
+			$newStock = 0;
+		} else {
+			$newStock = $row['Stock'] - $qty;
+			echo $newStock;
+			//echo "<script>alert(\"Thank you!\");</script>";
+		}
+		//update how many of product are left
+		$update = mysql_query("UPDATE PRODUCTS SET Stock='".$newStock."' WHERE ProductID='".$productid."';");
+		echo $row['Name'].", ".$row['Stock']."<br>";
 	}
-	//update how many of product are left
-		//if user exceeds amount, write alert
+	
+		
 }
-	//write thank you
+	$_SESSION['cart'] = array();
+	echo "<script>alert(\"Thank you for shopping with us!\");</script>";
 ?>
