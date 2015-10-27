c0f4a083352f0eaa0e3d7d022c906b3c170481e6
Sarah Hendricks <slynnhendricks@gmail.com>
2015-10-27 11:17:29 +0800

    Remove unnecessary files for project submission

--- Assignment 2/users.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE USERS (Username CHAR(25) NOT NULL, Password VARCHAR(100) NOT NULL, FirstName CHAR(20), 
-	LastName CHAR(30), Email VARCHAR(50), AddressLine1 VARCHAR(50), AddressLine2 VARCHAR(50),
-	City CHAR(20), State CHAR(3), Zip CHAR(4), StaffMember CHAR(1),
-	PRIMARY KEY (Username));
-
-INSERT INTO USERS VALUES ('annamarie', 'password', 'Anna', 'Marie', 'annamarie@gmail.com', '123 South St', '', 'Murdoch', 'WA', '6150', 'Y');
